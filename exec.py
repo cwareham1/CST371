06f4b5e7ac0a237df26a46f8010ae68cc591a0e5
ColinWareham <cwareham704@marian.edu>
2022-02-21 14:37:25 -0500

    added exporter classes for tyner and joes, changed persistence and exec

---
+++ exec.py
@@ -0,0 +1,8 @@
+# from Export import Requests_practice
+# from Export import tynerpond
+# from tynerpond import main
+from Export.tynerpond import main as tp
+from Export.joesshop import main as joe
+
+tp()
+joe()
