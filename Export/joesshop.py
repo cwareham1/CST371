c2dd4ca72ce68770636447983be478668e59bf33
ColinWareham <cwareham704@marian.edu>
2022-03-23 13:59:45 -0400

    test #3

--- Export/joesshop.py
+++ Export/joesshop.py
@@ -5,7 +5,5 @@ def regex(html):
     rawlist = re.findall(
         r"<p>([a-zA-Z\s\u2019\u002D\(\)\u20130-9]*).*?<strong>.*?([0-9]{1,2}\.[0-9]{2}.{0,5}).*?</strong>(?:.*?([0-9]{1,2}\.[0-9]{2})*.*?)*</p>"
         ,html)
-
-    return rawlist
-    
+    return rawlist #,
     
